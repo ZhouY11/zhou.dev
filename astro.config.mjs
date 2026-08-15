a7c70b389713ade94d81c919b11c6a8082e3fabb
ZhouY11 <zhiyi.zhou.cn@outlook.com>
2026-08-15 10:04:44 +0800

    feat(analytics): add Vercel Web Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,16 @@
 
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import { visualizer } from 'rollup-plugin-visualizer';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zhou-dev.vercel.app',
+
+  output: 'static',
+  adapter: vercel(),
 
   vite: {
     plugins: [
